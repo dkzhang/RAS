07098c54ec563552d58d481c38cfedf42e751966
dkzhang@Office.PC <527656264@qq.com>
2020-04-03 08:08:15 +0800

    modify passwd management.

--- toVncServer/vncOperation.go
+++ toVncServer/vncOperation.go
@@ -1,33 +1,23 @@
 package toVncServer
 
 import (
+	"RAS/myUtils/genPasswd"
 	"RAS/personDB"
 	"fmt"
-	"regexp"
+	"log"
 )
 
 func ModifyVncPassword(person personDB.Person, server SshServerInfo) (passwd string, err error) {
-	cmdFormat := `su - %s -c "/opt/TurboVNC/bin/vncpasswd -o -display :%d"`
-	cmd := fmt.Sprintf(cmdFormat, person.ServerUser, person.VncDisplay)
-	fmt.Println(cmd)
-	result, err := SshTo(server, cmd)
+	genPasswd.RandomSeed()
+	passwd = genPasswd.GeneratePasswd(8, genPasswd.FlagNumber)
 
-	//test
-	//reg := `^Full control one-time password: (\S+)$`
-	r := regexp.MustCompile(`Full control one-time password: (\S+)`)
+	cmdFormat := `/root/ras/setvncpasswd %s %s`
+	cmd := fmt.Sprintf(cmdFormat, person.ServerUser, passwd)
+
+	result, err := SshTo(server, cmd)
 	if err != nil {
 		return "", fmt.Errorf("SshTo server %s error:%v", server.Host, err)
 	}
-
-	if r.MatchString(result) == false {
-		return "", fmt.Errorf("server %s result %s, match failed", server.Host, result)
-	}
-
-	ms := r.FindStringSubmatch(result)
-	if len(ms) != 2 {
-		return "", fmt.Errorf("server %s result %s, match len error", server.Host, result)
-	}
-
-	return ms[1], nil
-	return
+	log.Printf("SshTo server %s success:%v", server.Host, result)
+	return passwd, nil
 }
