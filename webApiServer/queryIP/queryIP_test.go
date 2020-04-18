1a4c04e7171b607303a0dec633fc12a0ff2de766
dkzhang <dk@home.pc>
2020-04-18 18:33:21 +0800

    add queryIP_test

--- webApiServer/queryIP/queryIP_test.go
+++ webApiServer/queryIP/queryIP_test.go
@@ -1,11 +1,22 @@
 package queryIP
 
-import "testing"
+import (
+	"encoding/json"
+	"testing"
+)
 
 func TestQueryIP(t *testing.T) {
 	r, err := queryIP("27.187.116.121")
 	if err != nil {
 		t.Errorf("queryIP error: %v", err)
 	}
-	t.Logf("queryIP success: %v", r)
+	t.Logf("queryIP success")
+
+	ipInfoStruct := QueryIpApiResult{}
+	if err := json.Unmarshal(r, &ipInfoStruct); err != nil {
+		t.Errorf("ip info unmarshal error: %v", err)
+	} else {
+		//正常情况
+		t.Logf("ip info unmarshal reslut: %v", ipInfoStruct)
+	}
 }
