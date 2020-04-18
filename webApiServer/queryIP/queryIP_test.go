04ba92a7b08173cd16eb4868582c6522ea478b00
dkzhang <dk@home.pc>
2020-04-18 18:27:17 +0800

    add queryIP_test

--- webApiServer/queryIP/queryIP_test.go
+++ webApiServer/queryIP/queryIP_test.go
@@ -3,7 +3,7 @@ package queryIP
 import "testing"
 
 func TestQueryIP(t *testing.T) {
-	r, err := queryIP(" 27.187.116.121")
+	r, err := queryIP("27.187.116.121")
 	if err != nil {
 		t.Errorf("queryIP error: %v", err)
 	}
