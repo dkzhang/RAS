4b8e9fe5491639a2c640d369230792aea0e19415
dkzhang@Office.PC <527656264@qq.com>
2021-11-18 15:15:49 +0800

    add SshIP to the server struct.

--- myPostgreSQL/mySqlx_test.go
+++ myPostgreSQL/mySqlx_test.go
@@ -3,7 +3,7 @@ package myPostgreSQL
 import "testing"
 
 func TestConnectToDatabase(t *testing.T) {
-	db, err := ConnectToDatabase("postgres", "user=postgres dbname=mydb password=Jim980911 sslmode=disable")
+	db, err := ConnectToDatabase("postgres", "user=postgres dbname=mydb password=111111 sslmode=disable")
 	if err != nil {
 		t.Errorf("ConnectToDatabase error: %v", err)
 	}
